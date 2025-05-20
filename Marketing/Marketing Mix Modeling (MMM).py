abfb01569818c4d03e415a9c3d86d03b2b66a0a5
HenrySchall <henrique.schall@outlook.com>
2025-05-19 23:26:09 -0300

    .

---
+++ Marketing/Marketing Mix Modeling (MMM).py
@@ -0,0 +1,47 @@
+########################
+### Install Packages ###
+########################
+
+import subprocess
+import sys
+
+def install_packages(pacotes):
+    for pacote in pacotes:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", pacote])
+
+# List of packages
+packages_list = ["numpy", "pandas", "matplotlib", "scipy", "seaborn","statsmodels", "plotly", "gurobipy",
+"yfinance", "scikit-learn", "panel", "datashader", "param", "colorcet", "transformers","einops","accelerate", 
+"bitsandbytes"]
+
+install_packages(packages_list)
+
+#####################
+### Load Packages ###
+#####################
+
+import pyomo.environ as pyo
+import gurobipy as gp
+import pandas as pd 
+import seaborn as sns
+import plotly.express as px
+import numpy as np
+import panel as pn 
+import seaborn.objects as so
+import matplotlib as mpl
+import colorcet as cc
+import matplotlib.pyplot as plt
+import math
+import datetime
+import param
+import sklearn
+import scipy
+import string
+import random
+import torch
+import os
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder, MinMaxScaler
+from sklearn.naive_bayes import GaussianNB as GNB
+
